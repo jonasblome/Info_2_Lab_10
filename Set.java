f1903a750d2190199923646a1095efd201b3bf20
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-06-17 14:40:18 +0200

    Added basic functionalities

--- Set.java
+++ Set.java
@@ -1,20 +1,70 @@
+import java.util.ArrayList;
+
 /**
  * This class recreates the behavior of a Set.
  * 
- * @author	___ & n-c0de-r
+ * @author 	jonasblome
+ * @author	n-c0de-r
  * @version	17.06.21
  */
 
-public class Set {
-	public void add() {
-		
+public class Set<type>{
+	
+	private ArrayList<type> set;
+	
+	public Set() {
+		set = new ArrayList<>();
 	}
 	
-	public void contains() {
-		
+	public void add(type e) {
+		if (contains(e)) {
+			set.add(e);
+		} else {
+			return;
+		}
 	}
 	
-	public void remove() {
+	public boolean contains(type e) {
+		if (set.contains(e)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void remove(type e) {
+		if (contains(e)) {
+			int i = set.indexOf(e);
+			set.remove(i);
+		} else {
+			return;
+		}
+	}
+	
+	public int length() {
+		return set.size();
+	}
+	
+	public String toString() {
+		String res = "";
+		for (type t : set) {
+			res += ", " + t;
+		}
+		return res;
+	}
+	
+	public type get(int i) {
+		return set.get(i);
+	}
+	
+	public Set<type> union(Set<type> sub) {
+		Set<type> res = this;
 		
+		for (int i = 0; i<sub.length(); i++) {
+			if (!contains(sub.get(i))) {
+				res.add(sub.get(i));
+			}
+		}
+		return res;
 	}
 }
