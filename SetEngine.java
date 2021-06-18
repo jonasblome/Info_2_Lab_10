cc7b61f8e636238b5961102761ebfd0f906b1d65
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-06-18 10:13:27 +0200

    More Clean-up & JDoc descriptions

--- SetEngine.java
+++ SetEngine.java
@@ -1,93 +1,124 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
 /**
- * The main part doing the calculations based on former labs.
+ * GUI for the calculator, based on the former labs.
  * 
  * @author	jonasblome
- * @author	n-c0de-r &
- * @version	17.06.21
+ * @author	n-c0de-r 
+ * @version	17.06.2021
  */
+public class SetGUI implements ActionListener {
 
-public class SetEngine {
+	private SetEngine calc;
+	private JFrame frame;
+	private JTextField setA;
+	private JTextField setB;
+	private JTextField result;
 
-	private Set<Character> A;
-	private Set<Character> B;	
-	private Set<Character> res;
-	
-	/**
-	 * Constructor of the SetEngine class.
-	 */
-	public SetEngine() {
-		res = new Set<>();
-		A = new Set<>();
-		B = new Set<>();
+	public SetGUI(SetEngine engine) {
+		calc = engine;
+		makeFrame();
+		frame.setVisible(true);
 	}
-	
+
 	/**
-	 * Unites two given Sets to a new Set.
+	 * Set the visibility of the interface.
 	 * 
-	 * @param setA	The main set.
-	 * @param setB	The second set to merge to the first.
-	 * @return	Resulting Set merged from the given two.
+	 * @param visible true if the interface is to be made visible, false otherwise.
 	 */
-	public String union(String setA, String setB) {
-		A = toSet(setA);
-		B = toSet(setB);
-		res = A.union(B);
-		return res.toString();
+	public void setVisible(boolean visible) {
+		frame.setVisible(visible);
 	}
-	
-	/**
-	 * Intersects two given Sets to a new Set.
-	 * 
-	 * @param setA	The main set.
-	 * @param setB	The second set to check for intersection with the first.
-	 * @return	Resulting Set of intersecting from the given two.
-	 */
-	public String intersection(String setA, String setB) {
-		A = toSet(setA);
-		B = toSet(setB);
-		res = A.intersection(B);
-		return res.toString();
+
+	private void makeFrame() {
+		frame = new JFrame("Set Calculator");
+
+		JPanel contentPane = (JPanel) frame.getContentPane();
+		contentPane.setLayout(new BorderLayout(8, 8));
+		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+		JPanel inputPanel = new JPanel(new GridLayout(2, 1));
+		
+		setA = new JTextField();
+		inputPanel.add(setA, BorderLayout.NORTH);
+		setB = new JTextField();
+		inputPanel.add(setB, BorderLayout.SOUTH);
+		
+		contentPane.add(inputPanel, BorderLayout.NORTH);
+		
+		JPanel buttonPanel = new JPanel(new GridLayout(2, 3));
+
+		addButton(buttonPanel, "Union");
+		addButton(buttonPanel, "Intersection");
+		addButton(buttonPanel, "Subtraction");
+
+		addButton(buttonPanel, "Length Set A");
+		addButton(buttonPanel, "Length Set B");
+		addButton(buttonPanel, "Clear Fields");
+
+		contentPane.add(buttonPanel, BorderLayout.CENTER);
+		
+		result = new JTextField();
+		contentPane.add(result, BorderLayout.SOUTH);
+		result.setEditable(false);
+
+		frame.pack();
 	}
-	
+
 	/**
-	 * Subtracts one Set from another.
+	 * Performs an action according to the String labeling a button.
 	 * 
-	 * @param setA	The main set.
-	 * @param setB	The second set to subtract from the first.
-	 * @return	Resulting Set of subtracting from the given two.
+	 * @param event	The event causing the action, a button click.
 	 */
-	public String subtraction(String setA, String setB) {
-		A = toSet(setA);
-		B = toSet(setB);
-		res = A.subtraction(B);
-		return res.toString();
-	}
-	
-	/**
-	 * Getter method to return the number of elements of a Set.
-	 * 
-	 * @param set	Given Set to check the number of elements.
-	 * @return	String containing the number of elements of a Set.
-	 */
-	public String length(String set) {
-		A = toSet(set);
-		int len = A.length();
-		return Integer.toString(len);
-	}
-	
-	/**
-	 * Setter method to turn given Strings to a Set
-	 * 
-	 * @param str	String to be converted.
-	 * @return	Resulting Set of Characters of the input String.
-	 */
-	private Set<Character> toSet(String str) {
-		Set<Character> set = new Set<>();
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) != ',' && str.charAt(i) != ' ') {
-			set.add(str.charAt(i));
-			}
+	public void actionPerformed(ActionEvent event) {
+		String command = event.getActionCommand();
+		String A = setA.getText();
+		String B = setB.getText();
+		String str = "";
+		if (command.equals("Union")) {
+			str = calc.union(A, B);
+		} else if (command.equals("Intersection")) {
+			str =  calc.intersection(A, B);
+		} else if (command.equals("Subtraction")) {
+			str =  calc.subtraction(A, B);
+		} else if (command.equals("Length Set A")) {
+			str = "Length of Set A is :" + calc.length(A);
+		} else if (command.equals("Length Set B")) {
+			str = "Length of Set B is :" + calc.length(B);
+		} else if (command.equals("Clear Fields")) {
+			clear();
 		}
-		return set;
+		result.setText(str);
+	}
+
+	/**
+	 * Add a button to the button panel.
+	 * 
+	 * @param panel      The panel to receive the button.
+	 * @param buttonText The text for the button.
+	 */
+	protected void addButton(Container panel, String buttonText) {
+		JButton button = new JButton(buttonText);
+		button.addActionListener(this);
+		panel.add(button);
+	}
+	
+	/**
+	 * Clears all input and output fields.
+	 */
+	private void clear() {
+		setA.setText("");
+		setB.setText("");
+		result.setText("");
 	}
 }
