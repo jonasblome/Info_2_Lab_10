2686170d7f342b23aea4a1e6dc1cf77147ebf085
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-06-18 10:11:24 +0200

    More Clean-up & JDoc descriptions

--- Set.java
+++ Set.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 /**
  * This class recreates the behavior of a Set.
+ * Also calculates different subsets of given Sets.
  * 
  * @author 	jonasblome
  * @author	n-c0de-r
@@ -12,10 +13,20 @@ public class Set<type>{
 	
 	private ArrayList<type> set;
 	
+	/**
+	 * Constructor for the Set class.
+	 */
 	public Set() {
 		set = new ArrayList<>();
 	}
 	
+	// Basic data methods start here
+	
+	/**
+	 * Setter method for Set.
+	 * 
+	 * @param e	Adding an element to the Set.
+	 */
 	public void add(type e) {
 		if (!contains(e)) {
 			set.add(e);
@@ -24,6 +35,12 @@ public class Set<type>{
 		}
 	}
 	
+	/**
+	 * Getter method, checking if an element is already in the Set.
+	 * 
+	 * @param e	Element to be inserted into the Set.
+	 * @return	True if the element is already in the Set.
+	 */
 	public boolean contains(type e) {
 		if (set.contains(e)) {
 			return true;
@@ -32,6 +49,30 @@ public class Set<type>{
 		}
 	}
 	
+	/**
+	 * Getter method, returning element at a position.
+	 * 
+	 * @param i	Index number to get the element at.
+	 * @return	Return the Object of the given type.
+	 */
+	public type get(int i) {
+		return set.get(i);
+	}
+	
+	/**
+	 * Getter method for number of elements in the Set
+	 * 
+	 * @return	Integer number of elements.
+	 */
+	public int length() {
+		return set.size();
+	}	
+	
+	/**
+	 * Setter method to remove an element from the Set.
+	 * 
+	 * @param e	Element to be removed from the Set.
+	 */
 	public void remove(type e) {
 		if (contains(e)) {
 			int i = set.indexOf(e);
@@ -41,22 +82,29 @@ public class Set<type>{
 		}
 	}
 	
-	public int length() {
-		return set.size();
-	}
-	
+	/**
+	 * Convert all element as a String
+	 * 
+	 * @return	Resulting String of elements in curly braces
+	 */
 	public String toString() {
-		String res = "";
+		String res = "{ ";
 		for (type t : set) {
 			res += t + ", ";
 		}
+		// Remove the last instance of ', ' adding closing parentheses
+		res = res.substring(0, res.length()-2) + " }";
 		return res;
 	}
 	
-	public type get(int i) {
-		return set.get(i);
-	}
+	//Calculation methods start here
 	
+	/**
+	 * Setter method, unite two given Sets together.
+	 * 
+	 * @param sub	The second Set to merge with the main one.
+	 * @return	Resulting Set, combination of both.
+	 */
 	public Set<type> union(Set<type> sub) {
 		Set<type> res = this;
 		
@@ -68,9 +116,15 @@ public class Set<type>{
 		return res;
 	}
 	
+	/**
+	 * Setter method, create in intersection of two Sets.
+	 * 
+	 * @param sub	The second Set to find the intersection with the main one.
+	 * @return	Resulting Set, intersection of both.
+	 */
 	public Set<type> intersection(Set<type> sub) {
 		Set<type> res = this;
-//		
+		
 //		for (int i = 0; i<sub.length(); i++) {
 //			if (!contains(sub.get(i))) {
 //				res.add(sub.get(i));
@@ -79,9 +133,32 @@ public class Set<type>{
 		return res;
 	}
 	
+	/**
+	 * Setter method, create in subtraction of two Sets.
+	 * 
+	 * @param sub	The second Set to subtract from the main one.
+	 * @return	Resulting Set, subtraction of both.
+	 */
 	public Set<type> subtraction(Set<type> sub) {
 		Set<type> res = this;
-//		
+		
+//		for (int i = 0; i<sub.length(); i++) {
+//			if (!contains(sub.get(i))) {
+//				res.add(sub.get(i));
+//			}
+//		}
+		return res;
+	}
+	
+	/**
+	 * Setter method, create a power Set of two given Sets.
+	 * 
+	 * @param sub	The second Set to combine with the main one.
+	 * @return	Resulting Set, permutation of both.
+	 */
+	public Set<type> powerset(Set<type> sub) {
+		Set<type> res = this;
+		
 //		for (int i = 0; i<sub.length(); i++) {
 //			if (!contains(sub.get(i))) {
 //				res.add(sub.get(i));
